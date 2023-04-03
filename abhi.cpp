c67249806fd75690ef261f80ed9a1ba312d484b0
abhi12tyagi <128505893+abhi12tyagi@users.noreply.github.com>
2023-04-04 00:30:15 +0530

    Add files via upload

---
+++ abhi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+   for (int i = 0; i < a; i++)
+   {
+   cout <<i;
+   cout<<endl;hello abhi
+   }
+   
+    
+    return 0;
+}
